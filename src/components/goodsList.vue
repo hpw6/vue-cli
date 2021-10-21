0c6a2affc7c938cf655d786b39d6e7ebc3fd52de
hpinwei <hpinwei@linewell.com>
2021-10-21 09:42:26 +0800

    first commit

---
+++ src/components/goodsList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div name="show">
+        <ul>
+            <li v-for="good in list">
+                <img v-bind:src="good.img">
+                <p>{{good.goodName}}</p>
+            </li>
+
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "show",
+        data(){
+            var obj = this;
+            this.$http.get("json/dn.json").then(function (res) {
+                obj.list=res.data;
+            })
+            return{
+                list:[]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
